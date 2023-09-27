91450de8a207c871ce97f49136088f3992d7a033
Muhammad Asharul <masharul51@gmail.com>
2023-09-27 14:57:17 +0700

    add quis: for & slice

--- main.go
+++ main.go
@@ -132,13 +132,34 @@ func main() {
 	// fmt.Println(myMap)
 
 	// slice of map
-	students := []map[string]string{
-		{"name": "Arul", "score": "A"},
-		{"name": "Aziz", "score": "B"},
-		{"name": "Abi", "score": "C"},
-	}
-	for _, value := range students {
-		fmt.Println(value["name"], " - ", value["score"])
-	}
+	// students := []map[string]string{
+	// 	{"name": "Arul", "score": "A"},
+	// 	{"name": "Aziz", "score": "B"},
+	// 	{"name": "Abi", "score": "C"},
+	// }
+	// for _, value := range students {
+	// 	fmt.Println(value["name"], " - ", value["score"])
+	// }
 
+	// kuis
+	// 1. hitung rata-rata
+	scores := [8]int{100, 80, 75, 92, 70, 93, 88, 67}
+	// var total int
+	// for _, score := range scores {
+	// 	total = total + score
+	// }
+
+	// length := len(scores)
+	// average := float64(total) / float64(length)
+	// fmt.Println(average)
+
+	// 2. append to slice
+	var goodScores []int
+
+	for _, score := range scores {
+		if score >= 90 {
+			goodScores = append(goodScores, score)
+		}
+	}
+	fmt.Println(goodScores)
 }
