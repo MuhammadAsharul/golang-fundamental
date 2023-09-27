d9cd8b45f21bbb0bce74a2f06a9966d1a0fd106e
Muhammad Asharul <masharul51@gmail.com>
2023-09-27 13:52:02 +0700

    add: array

--- main.go
+++ main.go
@@ -6,17 +6,24 @@ import (
 )
 
 func main() {
+	// println
 	fmt.Println("Halo Saya belajar Golang")
+
+	// import func from deferent package
 	result := calculation.Add(11, 2)
 	fmt.Println(result)
 
 	result1 := calculation.Multiply(11, 2)
 	fmt.Println(result1)
 
+	// declare variable
+
 	// var name string = "Golang"
 	// atau
 	name := "Gojek"
 	fmt.Println(name)
+
+	// if else
 
 	// age := 9
 	// if age >= 10 {
@@ -24,6 +31,8 @@ func main() {
 	// } else {
 	// 	fmt.Println("Kamu belum boleh bermain game")
 	// }
+
+	// switch
 
 	// score := 71
 	// var grade string
@@ -52,6 +61,7 @@ func main() {
 	// 	fmt.Println("default")
 	// }
 
+	// for
 	// for i := 0; i <= 100; i++ {
 	// 	fmt.Println("Halo", i)
 	// }
@@ -61,22 +71,42 @@ func main() {
 	// 	j++
 	// }
 
-	title := "Golang is the best language"
+	// for text
+	// title := "Golang is the best language"
 	// for index, letter := range title {
 	// 	if index %2 == 0 {
 	// 		fmt.Println("index :",index,"Letter :", string(letter))
 	// 	}
 	// }
 
-	for index, letter := range title {
-		stringLetter := string(letter)
-		// if stringLetter == "o" || stringLetter == "a" || stringLetter == "i" || stringLetter == "e" || stringLetter == "u" {
-		// 	fmt.Println("index :", index, "letter", stringLetter)
-		// }
-		
-		switch stringLetter {
-		case "a", "i", "u", "e", "o":
-			fmt.Println("index :", index, "letter", stringLetter)
-		}
+	// for index, letter := range title {
+	// 	stringLetter := string(letter)
+	// 	// if stringLetter == "o" || stringLetter == "a" || stringLetter == "i" || stringLetter == "e" || stringLetter == "u" {
+	// 	// 	fmt.Println("index :", index, "letter", stringLetter)
+	// 	// }
+
+	// 	switch stringLetter {
+	// 	case "a", "i", "u", "e", "o":
+	// 		fmt.Println("index :", index, "letter", stringLetter)
+	// 	}
+	// }
+
+	// array
+
+	// var languages [5]string
+	// languages[0] = "Golang"
+	// languages[1] = "Ruby"
+	// languages[2] = "C++"
+	// languages[3] = "PHP"
+	// languages[4] = "Javascript"
+
+	languages := [...]string{"Golang", "Ruby", "C++", "PHP", "Javascript", "Python"}
+	fmt.Println(languages)
+	length := len(languages)
+	fmt.Println(length)
+
+	for index, lang := range languages {
+		fmt.Println("index", index, "language", lang)
 	}
+
 }
