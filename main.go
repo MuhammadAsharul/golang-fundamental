09b0bd6f42745985a4dc59d4911bb5a811ae8183
Muhammad Asharul <masharul51@gmail.com>
2023-10-02 10:06:29 +0700

    add: pointer advance

--- main.go
+++ main.go
@@ -214,16 +214,28 @@ func main() {
 	// }
 
 	// pointer
-	numberA := 5
-	numberB := &numberA
+	// numberA := 5
+	// numberB := &numberA
+
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
+
+	// *numberB = 10
+	// fmt.Println(*numberB)
+	// fmt.Println(numberA)
+
+	var numberA int = 5
+	var numberB *int = &numberA
 
 	fmt.Println(numberA)
 	fmt.Println(numberB)
 	fmt.Println(*numberB)
 
-	*numberB = 10
-	fmt.Println(*numberB)
+	numberA = 20
 	fmt.Println(numberA)
+	fmt.Println(numberB)
+	fmt.Println(*numberB)
 
 }
 
